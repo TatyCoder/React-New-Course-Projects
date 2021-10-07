35035f64ad225d6b0a29691380eff54c7dee03b0
TatyCoder <tmilanes@gmail.com>
2021-10-07 15:26:00 -0400

    Practice: adding ErrorModal component

--- Section 8/01-starting-project/src/Components/Users/AddUser.js
+++ Section 8/01-starting-project/src/Components/Users/AddUser.js
@@ -7,6 +7,9 @@ import Card from '../UI/Card';
 // To import the Button component to use it here:
 import Button from '../UI/Button';
 
+// To import the ErrorModal component to use it here:
+import ErrorModal from '../UI/ErrorModal';
+
 // To import the styles using CSS modules:
 import styles from './AddUser.module.css'
 
@@ -21,7 +24,8 @@ const AddUser = (props) => {
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
     }
-    if (+enteredAge < 1) {  // By adding a + it forces a conversion of enteredAge to a number (not a string).
+    if (+enteredAge < 1) {
+      // By adding a + it forces a conversion of enteredAge to a number (not a string).
       return;
     }
     // Calling the new props here and executing it as a function forwarding the two pieces of data:
@@ -40,28 +44,36 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
-  // Adding the value props to reflect my current state in the inputs when the form is submitted:
+  // Adding the ErrorModal component here:
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input 
-          id="username" 
-          type="text" 
-          value={enteredUsername} 
-          onChange={usernameChangeHandler} 
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input 
-          id="age" 
-          type="number" 
-          value={enteredAge} 
-          onChange={ageChangeHandler} 
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!"/>
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
 export default AddUser;
+
+/* Notes: Since React doesn't allow two side by side components 
+being rendered directly as a root element of my return statement, 
+I needed to wrap ErrorModal & Card in an extra div. Inside of a 
+div I can have adjacent elements. */
